81489612bb8c1767ae014b1227c9352a5ec0c927
Erind Hoxha <932400893@cit3-501m-20.local>
2019-03-20 11:19:23 +1300

    added the handlebars

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
+//ERINDS CODE FOR FACTS
+
 var url = "https://api.nasa.gov/planetary/apod?api_key=gdboBB95GZvKSjHc4gj10IOwU8jqJIaRv5XuuDjg";
-
-
 $.ajax({
   url: url,
   success: function(result){
@@ -26,3 +26,24 @@ $.ajax({
   $("#apod_title").text(result.title);
 }
 });
+
+$("#search").on('click', function(e) {
+    location.replace(`facts.html?id=${searchedValue}`)
+    var pageURL = new URL(document.location);
+    var params = pageURL.searchParams;
+    var id = params.get('id');
+
+    var url = "https://images-api.nasa.gov/search?q=";
+    var searchedValue = $("#search-input").val();
+    fetch(`${url}${searchedValue}`)
+    .then((response) => response.json())
+    .then((jsonresponse) => {
+        console.log(jsonresponse);
+        var response = jsonresponse.collection.items;
+        console.log(response);
+    });
+
+})
+
+
+// ERINDS CODE FOR FACTS
