bca694b757179578c2cb235ae173aedaa74ac0d3
Erind Hoxha <932400893@cit3-501m-20.local>
2019-03-21 11:41:44 +1300

    added

--- js/main.js
+++ js/main.js
@@ -30,23 +30,18 @@
     // NAV BAR
 
   // Get the input field
-  var input = document.getElementById("#search");
+  // var input = document.getElementById("#search");
 
-  // Execute a function when the user releases a key on the keyboard
-  input.addEventListener("keyup", function(event) {
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.keyCode === 13) {
-      // Cancel the default action, if needed
-      event.preventDefault();
-      // Trigger the button element with a click
-      document.getElementById("myBtn").click();
-    }
-  });
-
-    theToggle.onclick = function () {
-      toggleClass(this, 'on');
-      return false;
-    }
+  // // Execute a function when the user releases a key on the keyboard
+  // input.addEventListener("keyup", function(event) {
+  //   // Number 13 is the "Enter" key on the keyboard
+  //   if (event.keyCode === 13) {
+  //     // Cancel the default action, if needed
+  //     event.preventDefault();
+  //     // Trigger the button element with a click
+  //     document.getElementById("myBtn").click();
+  //   }
+  // });
 
     //Slick Slider
     $(document).ready(function(){
