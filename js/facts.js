5a7c81c3cd0c8b5221dfd151561875efbcbd8dad
Erind Hoxha <erind_cbh@hotmail.com>
2019-03-20 20:34:10 +1300

    added the search functionality

---
+++ js/facts.js
@@ -0,0 +1,66 @@
+
+var url = "https://api.nasa.gov/planetary/apod?api_key=gdboBB95GZvKSjHc4gj10IOwU8jqJIaRv5XuuDjg";
+$.ajax({
+  url: url,
+  success: function (result) {
+    console.log
+    if ("copyright" in result) {
+      $("#copyright").text("Image Credits: " + result.copyright);
+    } else {
+      $("#copyright").text("Image Credits: " + "Public Domain");
+    }
+
+    if (result.media_type == "video") {
+      $("#apod_img_id").css("display", "none");
+      $("#apod_vid_id").attr("src", result.url);
+    } else {
+      $("#apod_vid_id").css("display", "none");
+      $("#apod_img_id").attr("src", result.url);
+    }
+    $("#reqObject").text(url);
+    $("#returnObject").text(JSON.stringify(result, null, 4));
+    $("#apod_explaination").text(result.explanation);
+    $("#apod_title").text(result.title);
+  }
+});
+  var pageURL = new URL(document.location);
+  var params = pageURL.searchParams;
+  var searchedValue = params.get('searched');
+  searchClicked = searchedValue;
+
+if (searchClicked == "true") {
+  var pageURL = new URL(document.location);
+  var params = pageURL.searchParams;
+  var searchedValue = params.get('search');
+  var url = "https://images-api.nasa.gov/search?q=";
+  console.log(searchedValue);
+
+  fetch(`${url}${searchedValue}`)
+    .then((response) => response.json())
+    .then((jsonresponse) => {
+      var response = jsonresponse.collection;
+      console.log(response);
+      var source = document.getElementById("planet-template").innerHTML;
+      var template = Handlebars.compile(source);
+      var context = (response);
+      var html = template(context);
+      $("#facts").html(html);
+    });
+} else {
+  var url = "https://images-api.nasa.gov/search?q=mars";
+  fetch(`${url}`)
+    .then((response) => response.json())
+    .then((jsonresponse) => {
+      var response = jsonresponse.collection;
+      console.log(response);
+      var source = document.getElementById("planet-template").innerHTML;
+      var template = Handlebars.compile(source);
+      var context = (response);
+      var html = template(context);
+      $("#facts").html(html);
+    });
+}
+
+
+
+// ERINDS CODE FOR FACTS
--- js/main.js
+++ js/main.js
@@ -1,56 +1,9 @@
-//ERINDS CODE FOR FACTS
-
-var url = "https://api.nasa.gov/planetary/apod?api_key=gdboBB95GZvKSjHc4gj10IOwU8jqJIaRv5XuuDjg";
-$.ajax({
-  url: url,
-  success: function(result){
-    console.log
-  if("copyright" in result) {
-    $("#copyright").text("Image Credits: " + result.copyright);
-  }
-  else {
-    $("#copyright").text("Image Credits: " + "Public Domain");
-  }
-  
-  if(result.media_type == "video") {
-    $("#apod_img_id").css("display", "none"); 
-    $("#apod_vid_id").attr("src", result.url);
-  }
-  else {
-    $("#apod_vid_id").css("display", "none"); 
-    $("#apod_img_id").attr("src", result.url);
-  }
-  $("#reqObject").text(url);
-  $("#returnObject").text(JSON.stringify(result, null, 4));  
-  $("#apod_explaination").text(result.explanation);
-  $("#apod_title").text(result.title);
-}
-});
-var clicked = false;
-var sentFromOtherPage;
-var itsFactsPage;
-$("#search").on('click', function(e) {
-        if ($("#facts").length == 0) {
-            window.location.href = 'facts.html';
-            sentFromOtherPage = true;
-        }
-
-            var searchedValue = $("#search-input").val();
-            var url = "https://images-api.nasa.gov/search?q=";
-            console.log(searchedValue);
-    
-            fetch(`${url}${searchedValue}`)
-            .then((response) => response.json())
-            .then((jsonresponse) => {
-                var response = jsonresponse.collection;
-                console.log(response);
-                var source   = document.getElementById("planet-template").innerHTML;
-                var template = Handlebars.compile(source);
-                var context = (response);
-                var html    = template(context);
-                $("#facts").html(html);
-            });
-})
-
-
-// ERINDS CODE FOR FACTS
+    // DEFINE GLOBAL VARIABLES HERE
+    var searchClicked = false;
+    $("#search").on('click', function(e) {
+        e.preventDefault();
+        var searchVal = $("#search-input").val();
+        window.location.href = `facts.html?search=${searchVal}&searched=true`;
+        searchClicked = true;
+        console.log(searchClicked);
+    })
