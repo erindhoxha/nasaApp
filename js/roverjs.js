def800128468fd97d7bf8c6512aeec6b630a997c
Ella madeleine lennox Stoner <845847778@cit3-501m-18.local>
2019-03-22 11:34:21 +1300

    Added a carosel slick slider will go here for dynamic data"

--- js/roverjs.js
+++ js/roverjs.js
@@ -1,37 +1,31 @@
-$(function(){
 
 
-var apiKey = "TBXJbvg5BOgWrUI5dgjrf23cS4Fhs1GczfDNJYEe";
-
-var urlRover = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=TBXJbvg5BOgWrUI5dgjrf23cS4Fhs1GczfDNJYEe";
-
-console.log(urlRover);
-
-$.ajax({
-
-  url: urlRover,
-
-  var image = res.photos;
-    for (var i = 0; i < image.length; i++) {
-      if (image) {
-        $('<img src="' + image.img_src + '"></img>').appendTo('.roverie');	
-        break;											
-      }
-    }
-
-  success: function(res){
-
-    console.log(res);
-
-    $
+// $(function(){
 
 
-  },
+// var apiKey = "TBXJbvg5BOgWrUI5dgjrf23cS4Fhs1GczfDNJYEe";
 
-}); // END ajax request
+// var urlRover = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=TBXJbvg5BOgWrUI5dgjrf23cS4Fhs1GczfDNJYEe";
+
+// console.log(urlRover);
+
+// $.ajax({
+
+//   url: urlRover,
+
+//   success: function(res){
+
+//     console.log(res);
+
+    
 
 
-});
+//   },
+
+// }); // END ajax request
+ 
+
+// });
 
 
 
@@ -40,11 +34,11 @@ $.ajax({
 
 
 
-var projects = res.projects;
-				for (var i = 0; i < projects.length; i++) {
-					if (projects[10]) {
-						$('<div style = \"background-image:url(\'' + projects[10].covers.original + '\');height:100vh;background-size:cover;background-repeat:no-repeat;"></div>').appendTo('.hero');	
-						break;											
-					}
-				}
+// var projects = res.projects;
+// 				for (var i = 0; i < projects.length; i++) {
+// 					if (projects[10]) {
+// 						$('<div style = \"background-image:url(\'' + projects[10].covers.original + '\');height:100vh;background-size:cover;background-repeat:no-repeat;"></div>').appendTo('.hero');	
+// 						break;											
+// 					}
+// 				}
 
