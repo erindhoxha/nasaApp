a2cf1222cc6d0998ade40df0cfd351bcba8f2f30
heathmcgowan <heathjmcgowan@gmail.com>
2019-03-22 11:34:01 +1300

    updated planets.js

--- js/planets.js
+++ js/planets.js
@@ -1,13 +1,16 @@
 var planetContainer = document.querySelector('.planet-container');
 
 planetContainer.onclick = function() {
-var selectedPlanet = this.dataset.planet;
-  var url = "https://images-api.nasa.gov/search?q=";
-  fetch(`${url}${selectedPlanet}`)
-    .then((response) => response.json())
-    .then((jsonresponse) => {
-      var response = jsonresponse.collection;
-      console.log(response);
-      console.log(response.items[0]);
+    var selectedPlanet = this.dataset.planet;
+    var url = "https://images-api.nasa.gov/search?q=";
+    var randomNum = Math.floor(Math.random() * 99);
+    console.log(randomNum);
+    fetch(`${url}${selectedPlanet}`)
+        .then((response) => response.json())
+        .then((jsonresponse) => {
+        var response = jsonresponse.collection;
+        console.log(response);
+        console.log(response.items[randomNum].links[0].href);
     });
-} 
+}
+
